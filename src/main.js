c16dcc08c2033bb6da1e65dba8d2032576096537
oleksandr_bondarenko <oleksandr_bondarenko2@epam.com>
2020-04-13 20:59:07 +0300

    feat: add cors config
    provide KOA_CORS_CONFIG

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
-const { mindPlatformKoa } = require('@mindjs/platform-koa');
+const { mindPlatformKoa, KOA_CORS_CONFIG } = require('@mindjs/platform-koa');
 
 const AppModule = require('./app/app.module');
 
-mindPlatformKoa().bootstrapModule(AppModule);
+mindPlatformKoa({
+  platformExtraProviders: [
+    {
+      provide: KOA_CORS_CONFIG,
+      useValue: {
+        allowMethods: 'GET,POST,PUT',
+        origin: '*',
+      },
+    },
+  ],
+}).bootstrapModule(AppModule);
